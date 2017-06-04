434951ad440ae75a1ab8bbe838a3f929bdd7ea02
Victor Paul Noel <dev.paul@live.com>
2017-06-04 06:54:48 +0000

    Updated with template

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import os
 import collections
 import pymongo
 import sys
+import re
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-from flask import Flask
+from flask import Flask, render_template
 from flask import jsonify
 from flask import request
 
@@ -31,17 +32,46 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/scrapynews'
 
 mongo = PyMongo(app)
 
-@app.route('/')
-def index():
+def query_all():
     articles = mongo.db.articles
     
     results = []
     
     for q in articles.find():
         data = convert(q)
-        results.append({'title':data['title'],'author':data['author'],'url':data['url'],'description':data['description']})
+        results.append({'title':data['title'],'author':data['author'],'url':data['url'],'description':clean_text(data['description'])})
+    
+    return results
+
+def clean_text(html):
+    tag_rmvr = re.compile('<.*?>')
+    rt = re.sub(tag_rmvr, '', html)
+    return rt
+
+@app.route('/')
+def index():
+    results = query_all()
+        
+    return render_template('index.html', out = results)
+
+@app.route('/all')
+def get_all():
+    results = query_all()
         
     return jsonify(results)
+    
+@app.route('/getbyarticlename/<title>', methods=['GET'])
+def get_by_articlename(title):
+    articles = mongo.db.articles
+    
+    rgx = re.compile('^'+title, re.IGNORECASE)
+    data = articles.find_one({'title':rgx});
+    if data:
+        result = {'title':data['title'],'author':data['author'],'url':data['url'],'description':data['description']}
+    else:
+        result = {'results':'No record found'}
+    
+    return jsonify(result)
     
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
